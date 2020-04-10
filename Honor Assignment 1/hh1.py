a693ccbf55f121d98daa1290237b69a398b845de
aw-aw <aw.antonoffwertheimer@gmail.com>
2020-04-10 13:56:47 -0700

    finished HA1

--- Honor Assignment 1/hh1.py
+++ Honor Assignment 1/hh1.py
@@ -1,7 +1,6 @@
 import random as R
 from timeit import default_timer as timer
 import matplotlib.pyplot as plt
-import numpy as np
 
 R.seed( 1 )
 
@@ -84,7 +83,7 @@ def timeSortHelperTim( f, threshold, small, large, grow, tests_per_size, density
         arr_sizes.append( arr_size )
         times.append( diff / tests_per_size )
 
-        print( "'N'=%d time=%s" %( arr_size, diff / tests_per_size ) )
+        print(f'N={arr_size} samples, time={diff / tests_per_size:.2f} seconds')
         arr_size *= grow
     return ( arr_sizes, times )
 
@@ -111,39 +110,89 @@ def timeSortHelper( f, data_type, small, large, grow, tests_per_size, density ):
         arr_sizes.append( arr_size )
         times.append( diff / tests_per_size )
 
-        print( "'N'=%d time=%s" %( arr_size, diff / tests_per_size ) )
+        print(f'N={arr_size} samples, time={diff / tests_per_size:.2f} seconds')
+        # print( "'N'=%d time=%s" %( arr_size, diff / tests_per_size ) )
 
         arr_size *= grow
     return ( arr_sizes, times )
 
-
-def timeSort( f, small, large, grow, tests_per_size, density ):
-    if f != timsort:
+def timeSort( func_name, small, large, grow, tests_per_size, density ):
+    sorts_dict = { "TimSort" : timsort, "Insertion Sort" : insertionSort, "Merge Sort" : mergeSort }
+    xs = []
+    ys = []
+    labels = []
+    linestyles = []
+    if func_name != "TimSort":
         print( "Sorting sorted input" )
-        sorted_x, sorted_y = timeSortHelper( f, 0, small, large, grow, tests_per_size, density )
+        sorted_x, sorted_y = timeSortHelper( sorts_dict.get( func_name, mergeSort ), 0, small, large, grow, tests_per_size, density )
+        xs.append( sorted_x )
+        ys.append( sorted_y )
+        labels.append( func_name + " - Sorted" )
+        linestyles.append( '-' )
         print( "Sorting reversed input" )
-        reversed_x, reversed_y = timeSortHelper( f, 1, small, large, grow, tests_per_size, density )
+        reversed_x, reversed_y = timeSortHelper( sorts_dict.get( func_name, mergeSort ), 1, small, large, grow, tests_per_size, density )
+        xs.append( reversed_x )
+        ys.append( reversed_y )
+        labels.append( func_name + " - Reversed" )
+        linestyles.append( '--' )
         print( "Sorting random input" )
-        random_x, random_y = timeSortHelper( f, 2, small, large, grow, tests_per_size, density )
+        random_x, random_y = timeSortHelper( sorts_dict.get( func_name, mergeSort ), 2, small, large, grow, tests_per_size, density )
+        xs.append( random_x )
+        ys.append( random_y )
+        labels.append( func_name + " - Random" )
+        linestyles.append( '-.' )
 
-        plt.plot( sorted_x, sorted_y, label = "Sorted Inputs", linestyle = '-' )
-        plt.plot( reversed_x, reversed_y, label = "Reversed Sorted Inputs", linestyle = '--' )
-        plt.plot( random_x, random_y, label = "Random Inputs", linestyle = "-." )
+    else:
+        thresholds = [ 1, 10, 100, 1000, 10000, 100000 ]
+        lstyles = ['-', '--', '-.' ]
+        for i in range( len( thresholds ) ):
+            print( f"Sorting with threshold { thresholds[ i ] }" )
+            random_x, random_y = timeSortHelperTim( sorts_dict.get( func_name, timsort ), thresholds[ i ], small, large, grow, tests_per_size, density )
+            xs.append( random_x )
+            ys.append( random_y )
+            labels.append( func_name + " - TH" + str( thresholds[ i ] ) )
+            linestyles.append( lstyles[ i % 3 ] )
+    return xs, ys, labels, linestyles
 
-        x = np.arange( 0, 10000 )
-        y = x * np.log2( x )
+def plotData( x_list, y_list, label_list, style_list, min, max ):
+    for i in range( len( x_list ) ):
+        plt.plot( x_list[ i ], y_list[ i ], label = label_list[ i ], linestyle = style_list[ i ] )
+    plt.xlim( min, max )
+    plt.xlabel( "Array Sizes" )
+    plt.ylabel( "Run Times (s)" )
+    plt.legend()
+    plt.show()
 
-        plt.plot( x, y, label = "O( n log( n ) )", linestyle = ":" )
-        plt.xlim( 10000, 1000000 )
-        plt.xlabel( "Input Sizes" )
-        plt.ylabel( "Run Times" )
-        plt.legend()
-        plt.show()
+xs = []
+ys = []
+labels = []
+styles = []
+small = 1
+large = 10000
 
-timeSort( mergeSort, 10000, 1000000, 2, 10, 2 )
+insert_xs, insert_ys, insert_labels, insert_ls = timeSort( "Insertion Sort", small, large, 2, 1, 2 )
+merge_xs, merge_ys, merge_labels, merge_ls = timeSort( "Merge Sort", small, large, 2, 1, 2 )
+tim_xs, tim_ys, tim_labels, tim_ls = timeSort( "TimSort", small, large, 2, 1, 2 )
 
+for i in range( len( insert_xs ) ):
+    xs.append( insert_xs[ i ] )
+    ys.append( insert_ys[ i ] )
+    labels.append( insert_labels[ i ] )
+    styles.append( insert_ls[ i ] )
 
+for i in range( len( merge_xs ) ):
+    xs.append( merge_xs[ i ] )
+    ys.append( merge_ys[ i ] )
+    labels.append( merge_labels[ i ] )
+    styles.append( merge_ls[ i ] )
 
+for i in range( len( tim_xs ) ):
+    xs.append( tim_xs[ i ] )
+    ys.append( tim_ys[ i ] )
+    labels.append( tim_labels[ i ] )
+    styles.append( tim_ls[ i ] )
+
+plotData( xs, ys, labels, styles, small, large )
 
 
 
